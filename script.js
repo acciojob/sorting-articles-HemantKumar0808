6664bb2d72caa3b81d23a58a63a7423e633055c6
Hemant Kumar <hemusharma10001@gmail.com>
2025-12-28 23:21:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -26,7 +26,7 @@ const sortedBands = bands.sort((a, b) => {
 });
 
 // render to DOM
-const ul = document.getElementById('band');
+const ul = document.getElementById('bands');
 
 sortedBands.forEach(band => {
   const li = document.createElement('li');
